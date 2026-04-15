eb55943a81f35f71f1cf2735b28fbb6908e3a206
Nguyễn Tấn Dũng <23130072@st.hcmuaf.edu.vn>
2026-04-15 18:51:07 +0700

    tạo ra code smells test

--- src/script.js
+++ src/script.js
@@ -1,1 +1,20 @@
 console.log("Hello");
+function test(){
+    var a=1;var b=2;var c=3;var d=4;var e=5;
+
+    if(a==1){
+        if(b==2){
+            if(c==3){
+                if(d==4){
+                    if(e==5){
+                        console.log("nested too deep");
+                    }
+                }
+            }
+        }
+    }
+
+    console.log(a+b+c+d+e);
+}
+
+test();
